430fc8777f49eeffed5605eacff9d790c8f95434
Mirsait Sabirzyanov <mirsait77777@gmail.com>
2023-12-01 17:47:10 +0300

    Initial Commit

---
+++ BookCase/BookCaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookCaseApp.swift
+//  BookCase
+//
+//  Created by Мирсаит Сабирзянов on 01.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct BookCaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
